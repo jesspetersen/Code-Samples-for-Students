7c20bbe4c9f12170d8ffe80832de17a93731ed0b
YokeswaranMP <42894772+YokeswaranMP@users.noreply.github.com>
2019-10-14 19:25:49 +0530

    Create List.py
    I have added  a sample List program in python 3

---
+++ Foundation/List.py
@@ -0,0 +1,6 @@
+#creating a empty List for later use 
+List = []
+#creating for loop for getting values for 2 to 50 with diffrents of 2
+for i in range(2,50,2):
+    List.append(i) #Use append library function for add looped values into the List 
+print(List)#Print the list 
